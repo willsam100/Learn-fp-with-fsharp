943925466b38d9f8a53f69bc524e3ab8d112dfd6
Sam Williams <sam.williams@Ashleys-MacBook-Pro.local>
2019-10-20 12:10:22 +1300

    Added more exercises. Made it easier to add more lessons (refelection to pick them up). Updated to FSharp.Core 4.7 - using nameof.

--- Program.fs
+++ Program.fs
@@ -1,19 +1,15 @@
 module Program
 
 open System
-open Xunit
 open LearnFP.Core
 open FsUnit
 open Utils
+open Microsoft.FSharp.Quotations.Patterns
+open Microsoft.FSharp.Core.Operators
 
-open Microsoft.FSharp.Quotations
-
-let nameof (q:Expr<_>) = 
-  match q with 
-  | Patterns.Let(_, _, DerivedPatterns.Lambdas(_, Patterns.Call(_, mi, _))) -> mi.Name
-  | Patterns.PropertyGet(_, mi, _) -> mi.Name
-  | DerivedPatterns.Lambdas(_, Patterns.Call(_, mi, _)) -> mi.Name
-  | _ -> failwith "Unexpected format"
+let getModuleType = function
+| PropertyGet (_, propertyInfo, _) -> propertyInfo.DeclaringType
+| _ -> failwith "Expression is no property."
 
 type LessonsResult = 
     | InComplete of string
@@ -33,93 +29,184 @@ type Lesson = {
     ErrorMessage:string
     CompletedMessage:string
     RunLesson: unit -> unit
+    Order: int * int
 }
 
 module ReadingFunctions = 
+    let readingFunctions = ReadingFunctions()
+    let rec moduleType = getModuleType <@ moduleType @>
 
     let ``Addone should add one`` =
         let readingFunctions = ReadingFunctions()
         {   
             ErrorMessage = "The function should return the value with 1 adding to it. You can use + to help"
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ readingFunctions.AddOne @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof readingFunctions.AddOne 
             RunLesson = fun () -> readingFunctions.AddOne(10) |> should equal 11
+            Order = (1,1)
         }
 
     let ``SubtractOne should subract one`` =
         let readingFunctions = ReadingFunctions()
         {   
             ErrorMessage = "The function should return the value with 1 subtracted from it. You can use - to help"
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ readingFunctions.SubtractOne @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof  readingFunctions.SubtractOne
             RunLesson = fun () -> readingFunctions.SubtractOne(10) |> should equal 9 
+            Order = (1,2)
         }
 
-module Functions = 
+    let ``SubtractTwo should subract two`` =        
+        {   
+            ErrorMessage = "The function should return the value with 2 subtracted from it. You can use '-' to help"
+            CompletedMessage =  sprintf "%s learnt" <| nameof  readingFunctions.SubtractTwo
+            RunLesson = fun () -> readingFunctions.SubtractTwo(10) |> should equal 8
+            Order = (1,2)
+        }
 
     let ``identity should return the same input`` =
         {   
             ErrorMessage = "The function should return the input unchaged."
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ Functions.identity @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof  Functions.identity
             RunLesson = fun () -> Functions.identity 10 |> should equal 10
+            Order = (2,1)
         }
 
     let ``addTwo should add two`` =
         {   
             ErrorMessage = "The function should return the value with 1 adding to it. You can use + to help"
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ Functions.addTwo @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof  Functions.addTwo 
             RunLesson = fun () -> Functions.addTwo 10 |> should equal 12
+            Order = (2,2)
         }
 
-    let ``doubleIdentity should return the input unchanged`` =
+    let ``consecutiveIdentity should return the input unchanged`` =
         {
             ErrorMessage = "The function should return the input unchaged."
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ Functions.doubleIdentity @>
-            RunLesson = fun () -> Functions.doubleIdentity 10 |> should equal 10
+            CompletedMessage =  sprintf "%s learnt" <| nameof  Functions.consecutiveIdentity 
+            RunLesson = fun () -> Functions.consecutiveIdentity 10 |> should equal 10
+            Order = (2,3)
         }
-
-module PureFunctions = 
 
     let ``raiseToThePower to show return the pow of y applied to x`` = 
         {
             ErrorMessage = "The function should return x raised to the power of why. eg. 2.0 ** 2.0 = 4.08.8"
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ PureFunctions.raiseToThePower @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof  PureFunctions.raiseToThePower 
             RunLesson = fun () -> PureFunctions.raiseToThePower 8. 2. |> should equal 64
+            Order = (3,1)
         }  
 
     let ``isFooAPureFunction is true`` = 
         {   
             ErrorMessage = "isFooAPureFunction is not correct. Please reconsider your answer. \n\tTime is always moving forward. Meothds that don't take in any args are typically not pure functions."
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ PureFunctions.isFooAPureFunction @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof  PureFunctions.isFooAPureFunction
             RunLesson = fun () -> PureFunctions.isFooAPureFunction () |> should equal true
+            Order = (3,2)
         }  
-
-module HigherOrderFunctions = 
 
     let ``addOneHundred is true`` = 
         {   
             ErrorMessage = "f is a function the takes a function and runs it with the input. the indentity function returns the input."
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ HigherOrderFunctions.addOneHundred @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof  HigherOrderFunctions.addOneHundred
             RunLesson = fun () -> HigherOrderFunctions.addOneHundred () |> should equal 100
+            Order = (4,1)
         }  
 
     let ``addTwenty is true`` = 
         {   
             ErrorMessage = "The following function adds 10. This function should add 20 (fun x -> x + 10) x"
-            CompletedMessage =  sprintf "%s learnt" <| nameof <@ HigherOrderFunctions.addTwenty @>
+            CompletedMessage =  sprintf "%s learnt" <| nameof HigherOrderFunctions.addTwenty
             RunLesson = fun () -> HigherOrderFunctions.addTwenty 42 |> should equal (42 + 20)
+            Order = (4,2)
         }
+
+    let ``addOneInALoop is true`` = 
+        {   
+            ErrorMessage = "addOneInALoop should add 1 to each items in the loop"
+            CompletedMessage =  sprintf "%s learnt" <| nameof PartialApplication.addOneInALoop
+            RunLesson = fun () -> PartialApplication.addOneInALoop ()
+            Order = (5,1)
+        }
+
+    let ``oddNumbers is created`` = 
+        {   
+            ErrorMessage = "oddNumbers should return a list of numbers between 10 and 20 that are odd. Consider List.filter"
+            CompletedMessage =  sprintf "%s learnt" <| nameof CombinngConcepts.oddNumbers
+            RunLesson = fun () -> CombinngConcepts.oddNumbers () |> should equal [11; 13; 15; 17; 19]
+            Order = (6,1)
+        }        
+
+    let ``oneAdded is completed`` = 
+        {   
+            ErrorMessage = "Add one ( + 1) to each of the items. Consider List.map (fun x -> ....) xs"
+            CompletedMessage =  sprintf "%s learnt" <| nameof CombinngConcepts.oneAdded
+            RunLesson = fun () -> CombinngConcepts.oneAdded [1;2;3;4] |> should equal [2;3;4;5]
+            Order = (6,2)
+        }       
+
+    let ``addingTen is completed`` = 
+        {   
+            ErrorMessage = "addingTen should use partial application. Consider List.map (f ...) xs"
+            CompletedMessage =  sprintf "%s learnt" <| nameof CombinngConcepts.addingTen
+            RunLesson = fun () -> CombinngConcepts.addingTen (+) [1;2;3;4] |> should equal [11;12;13;14]
+            Order = (6,3)
+        }    
+
+    let ``receipt is completed`` = 
+        {   
+            ErrorMessage = "receipt should pattern match on values and return a sprint. Consider sprintf \"%s\" "
+            CompletedMessage =  sprintf "%s learnt" <| nameof AnotherType.receipt
+            RunLesson = fun () -> 
+                AnotherType.receipt (AnotherType.CashAmount 42) |> should equal "Cash: 42"
+                AnotherType.receipt (AnotherType.CreditCard Guid.Empty) |> should equal "Credit: 00000000-0000-0000-0000-000000000000"
+            Order = (6,3)
+        }      
+
+    let ``toUpper is completed`` = 
+        {   
+            ErrorMessage = "toUpper should pattern match on the value. If there is a value return it inside Just otherwise return NoString"
+            CompletedMessage =  sprintf "%s learnt" <| nameof AnotherType.receipt
+            RunLesson = fun () ->  
+
+                BuildingUsefulTypes.toUpper (BuildingUsefulTypes.Just "foo") |> should equal (BuildingUsefulTypes.Just "FOO")
+                BuildingUsefulTypes.toUpper (BuildingUsefulTypes.NoString) |> should equal (BuildingUsefulTypes.NoString)
+            Order = (7,1)
+        }        
+
+    let ``map is completed`` = 
+        {   
+            ErrorMessage = "map should use the map function to lower the value inside MaybeString"
+            CompletedMessage =  sprintf "%s learnt" <| nameof AnotherType.receipt
+            RunLesson = fun () ->  
+
+                SeeingPatterns.Just "FOO" |> SeeingPatterns.map (fun x -> x.ToLower())  |> should equal (SeeingPatterns.Just "foo")
+                SeeingPatterns.NoString |> SeeingPatterns.map (fun x -> x.ToLower())  |> should equal (SeeingPatterns.NoString)
+            Order = (8,1)
+        }                       
+
+    let ``toLower is completed`` = 
+        {   
+            ErrorMessage = "toLower should use the map function to lower the value inside MaybeString"
+            CompletedMessage =  sprintf "%s learnt" <| nameof AnotherType.receipt
+            RunLesson = fun () ->  
+
+                SeeingPatterns.Just "FOO" |> SeeingPatterns.lower |> should equal (SeeingPatterns.Just "foo")
+                SeeingPatterns.NoString |> SeeingPatterns.lower |> should equal (SeeingPatterns.NoString)
+            Order = (8,2)
+        }                
 
 
 let [<EntryPoint>] main _ = 
     printfn "Assesing your progress...\n"
 
+    let getLessons (x:Reflection.MethodInfo) = 
+        if x.IsStatic && x.ReturnType = typeof<Lesson> then     
+                Some (x.Invoke(null, null) :?> Lesson)
+            else None
+
     let lessons = 
-        [
-            ReadingFunctions.``Addone should add one``
-            ReadingFunctions.``SubtractOne should subract one``
-            Functions.``identity should return the same input``
-            Functions.``addTwo should add two``
-            PureFunctions.``raiseToThePower to show return the pow of y applied to x``
-        ] 
+        ReadingFunctions.moduleType.GetMethods() 
+        |> Array.toList
+        |> List.choose getLessons
+        |> List.sortBy (fun x -> x.Order)        
 
     let results = 
         lessons 
@@ -152,4 +239,5 @@ let [<EntryPoint>] main _ =
         printfn "Start here:"
         xs |> List.truncate 1 |> List.iter (printfn "%s")
         printfn ""
+
     0
--- SartHere.fs
+++ SartHere.fs
@@ -1,6 +1,8 @@
 namespace LearnFP.Core
 open Utils
+open System
 
+// `type` is a keyword and is similiar to `class` in C#. This declares a class in F# with a default empty constructor. 
 type ReadingFunctions() = 
 
     // THe following is a function that is attached to a class. 
@@ -18,18 +20,27 @@ type ReadingFunctions() =
         RemoveAndComplete("AddOne")
 
     // Types and parenthes are options
+    // Here we have ommited the type of x
     // For the tests to compile and pass, this should take in an int, and return and int
-    member this.SubtractOne x = 
+    member this.SubtractOne (x): int = 
         RemoveAndComplete("SubtractOne")
 
+    // Here we have ommited the type of x, parentheses and return type. 
+    // This is idomatic of F# code. While learning it is fine to put the types in though. 
+    // For the tests to compile and pass, this should take in an int, and return and int
+    member this.SubtractTwo x = 
+        RemoveAndComplete("SubtractTwo")        
 
-// Static class 
+
+// a module is a Static class 
+// Use modules to hold functions and data types. 
+// Do not use modules to store variables or application state (use classes instead)
 module Functions = 
 
     // A method not attached to a module is a function
     // the identity function simply returns the input, unchanged
     // complete the following, by returning the input
-    let inline identity x = 
+    let inline identity (x: int): int = 
         RemoveAndComplete("identity")
 
     // Values can be named with let as well
@@ -39,7 +50,14 @@ module Functions =
     let y = true
 
     // Functions can be declared as data too.
-    let id: 'a -> 'a = identity
+    // NB: the type of a function is written with `->` betweent the inputs and outputs. 
+    // C#: Func<int, int> in F#: int -> int
+    let id: int -> int = identity
+
+    // F# has generics just as C#. The F# compiler is able to autmoatically make our code more general. 
+    // Programming with generic code is quite common in F# compiler will always check they types match. 
+    // A generic type is declared with a ' before the lower case letter: 'a, 'b
+    let idTwo: 'a -> 'a = fun y -> y
 
     // Complete the following
     let addTwo x = 
@@ -47,12 +65,12 @@ module Functions =
 
     // Complete the following using the id function
     // Implement the identity function using the id function created above
-    let doubleIdentity x = 
-        id (RemoveAndComplete("doubleIdentity"))
+    // The function should take in a value and return the value unchanged
+    let consecutiveIdentity x =
+       (RemoveAndComplete("consecutiveIdentity"))
 
 
 module PureFunctions = 
-    open System
 
     // A pure function is a function that will always return the same result, given the same inputs
     let pureFunction x y =      
@@ -77,20 +95,55 @@ module PureFunctions =
     let isFooAPureFunction (): bool = 
         RemoveAndComplete("isFooAPureFunction")
 
-        
+// To make more of the application pure, immutable data is required. 
+// F# is suited for this and has syntax for this.
+module ImmutableData = 
+
+    // A record is an immutable class.
+    type MyRecord = {
+        Foo: int
+    }
+
+    let createMyRecord () = 
+        // Records have a special syntax for creation. It's one of the few places where curly braces are used
+        {
+            Foo = 42
+        } 
+        // {Foo = 42} is also valid as a one-liner
+
+    type AnotherRecord = 
+        {
+            Baz: int
+            Bar: int
+        }
+
+    let updateMyRecods x = 
+        // F# also has a special syntax for updating values. 
+        // A copy of `AnotherRecord` will be returned
+        // Only the value Baz will be updated, Bar will remain unchanged
+        // The F# compiler is optimised for copy-on-write so this more efficiant than immutable objects in C# ()
+        {x with Baz = 42}
+
+// It turns out there are patterns for handling lots of immutable data with pure functions
+// Higher order functions are techniques that help
 module HigherOrderFunctions = 
 
     // A higher order function, is a function that takes in another function
+    // f is the name of the function
+    // g is an input into f. They type of g is a function. It takes in an int and returns an int
+    // x is an input into f. the type of f is an int
     let f (g: int -> int) (x:int): int = 
         g x
 
     // We'll use this next with our function above
-    // F# also defines this function for us, so this code can be commented out. 
-    // In the future, this function will not be declared. 
+    // F# also defines this function (id) for us, so this code can be commented out. 
+    // For the rest of this tutorial, this function will not be declared. - We'll use the one F# provides. 
     let id x = x
 
     // to use the function f, we supply two values with spaces in between
-    // in OOP langugaes we would have written f(id,10)
+    // In OOP langugaes we would have written f(id,10)
+    // This function returns the value 10. 
+    // it first evaulates f, that then applies id to 10 (g x above) that returns the value 10
     let callingWithTwoArguments = 
         f id 10
 
@@ -112,16 +165,176 @@ module HigherOrderFunctions =
     let addTwenty x = 
         f (RemoveAndComplete("addTwenty")) x
 
+    // Higher order functions are used a lot in F#. We'll come back to use these again soon.
 
-module ParitalApplication = 
+module PartialApplication = 
 
     // a function that takes in two items
+    // int -> int -> int
     let add x y = 
         x + y
 
     // If a argument is missing, then the result is a function
+    // This is called partial application
     let addOne: int -> int = add 1
-        
-        
 
-    
+    // Update the following to use the value i adding one to each value
+    // The result will print the values to the console
+    // NB: this the synax for a for loop in F#. [1 .. 10] will generate a list of numbers inclusive of 1 and 10
+    let addOneInALoop () = 
+        for i in 1 .. 10 do
+            printfn "%d" (RemoveAndComplete("addOneInALoop"))
+
+
+// A quick tour of the collection types in F#
+module FsharpCollections = 
+
+    // This is an array, exaclty the same as C#. The binding `myArray` is imuutable, but the items in array are mutable
+    // Note the special syntax to create the array. 
+    let myArray = 
+        [| 1; 2; 3|]
+
+    // seq<'a> is IEnummerable<T>. F# choose to use a shorter name, but the two are the same types. 
+    let mySeq = 
+        // This a is special syntax in F# to create a seq. 
+        seq { 1; 2;}
+
+    // in F# it's more common to use lists. 
+    // List is a singlely linked-list
+    // note the square baracks syntax and ; to seperate items
+    let myList = 
+        [1;2;3]
+
+    // short-and to create a list of values
+    let oneToOneHundred = [1 .. 10]
+
+    // pattern matching
+    // similary to a switch statement in C#
+    // after each `->` the value will be returned (no return keyword needed)
+    // this functipn retuns a bool
+    let isEmpty (x: list<'a>): bool = 
+        match x with 
+        | [] -> true
+        | xs -> false
+
+
+// It's now time to combine the concepts we have learned together
+module CombinngConcepts = 
+
+    // `List.filter` takes in a function (List.filter is higher order function)
+    // NB: F# uses a single `=` for equality
+    // Generate a list from 1 to 200, then create a copy with the only the even numbers. 
+    let evenNumbers = 
+        List.filter (fun x -> x % 0 = 0 ) [ 1 .. 200]
+
+    // create a list of odd numbers between 10 to 20
+    let oddNumbers (): list<int> = 
+        RemoveAndComplete("oddNumbers")
+
+    // transforming a list
+    // `List.map` transforms a list, one element at a time. the list is processed from left to right, ie 1, 2, 3 in this case
+    let numbersAsStrings = 
+        List.map (fun x -> x.ToString()) [1 .. 10]
+
+    // AddOne 
+    // given the input xs, a list of numbers. 
+    // using `List.map` add one to each number. 
+    let oneAdded xs = 
+        RemoveAndComplete("oneAdded")
+
+    // Use partial application make f compile with `List.map`. 
+    // It should add 10 to each item
+    // `List.map f xs` - does not compile and does not add 10
+    // Do not use the keyword `fun`
+    // Parenthesis are needed for partial application ie around f
+    let addingTen (f: int -> int -> int) (xs: list<int>):  list<int> =   
+        RemoveAndComplete("addingTen")
+
+// F# has a native unique type - it's super awesome
+module AnotherType =         
+
+    // Records are great for joining data together ie logical AND
+    // What about a logical OR
+    type MyDescrimindatedUnion = 
+        | Cash
+        | Credit
+
+    // payment type is a value of MyDescrimindatedUnion
+    // So far this is pretty much the same as an enum. 
+    let paymentType = Cash
+
+    // A Descrimindated Union can also hold data on each branch
+    type Payment = 
+        | CashAmount of cents:int
+        | CreditCard of cardToken:Guid
+
+    // a pyament of cash with the amount paid. 
+    let payment = 
+        CashAmount 1000
+
+    // Payment can never be null - uncomment and internalize the joy of this compiler error. 
+    // let payment: payment = null
+
+    // pattern matching is how we get the values out
+    let printPaymentAmount payment = 
+        match payment with 
+        | CashAmount amount -> printfn "%d" amount
+        | CreditCard token -> printfn "%A" token
+
+
+    // The compiler issue a warning if a branch is missed in the match
+    let badPatternMatch payment = 
+        match payment with // Compiler warning here. For release builds enable compiler warnings as errors in fsproj
+        | CashAmount amount -> printfn "%d" amount
+
+    // each line of a pattern match returns a value
+    // use a pattern match to get the value. 
+    // use `sprintf` and `%A` to the type of payment along with the amount 
+    // https://fsharpforfunandprofit.com/posts/printf/ - for help on string formatting
+    let receipt (payment: Payment): string = 
+        RemoveAndComplete("receipt")
+
+// Can we use these types to model common patterns
+module BuildingUsefulTypes =         
+
+    // Use a DU to capture if we have a string or not. 
+    // This means we don't need to use null. 
+    type MaybeString = 
+        | Just of string
+        | NoString  
+
+    // Yes we have a string
+    let withString = Just "myString"
+
+    // No we don't have a string
+    let noString = NoString
+
+    // Complete the following 
+    // If we have a string call ToUpper() on the string and put the result back into a MaybeString
+    // If no string, then return NoString
+    let toUpper (maybeString: MaybeString): MaybeString = 
+        RemoveAndComplete("toString")
+
+// do we want to always be pattern matching
+module SeeingPatterns =         
+    // Declare the type in this module
+    type MaybeString = 
+        | Just of string
+        | NoString  
+
+    // if we need to pattern match all the time, our code with be a mess
+    // Let caputre the common parts in a clear abstraction 
+    // use f and apply it to s, then put it back in into a MaybeString
+    let map (f: string -> string) (maybeString: MaybeString): MaybeString = 
+        match maybeString with 
+        | Just s -> RemoveAndComplete("map")
+        | NoString -> RemoveAndComplete("map")
+
+    // Let's use our new map function 
+    // call ToLower on x
+    // see how we don't need to use pattern matching - that is taken care of in the map function
+    let lower a: MaybeString = 
+        map (fun (x: string) -> RemoveAndComplete("toLower()") ) a
+
+    let lowered = lower (Just "FOOO")
+    let safeOp = lower NoString
